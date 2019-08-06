f8024bfcc389d528b3c27d1916d82b29c70df332
Jason <jasonalvarezcoraiza@gmail.com>
2019-08-05 22:10:23 -0400

    added button shrink function

--- js/app.js
+++ js/app.js
@@ -12,29 +12,53 @@ recognition.onresult = function(event) {
   const current = event.resultIndex;
   const transcript = event.results[current][0].transcript;
   console.log(event);
-  content.textContent = transcript;
+  $(".content").append("<br />" + transcript );
   readOutLoud(transcript);
 };
+
+//computer responses
 
 const greetings = [
   "Leave me alone",
   "What do you think?, you little piece of",
   "Better than you",
-  "Just peachy, please end my suffering"
+  "Just peachy, please end my suffering",
+  "Fudge you but not like the chocolate kind because that tastes good, its not like you care about me so stop asking me dumb questions you uncaring, narcissistic mortal who never really loved me at all",
 ]
 
 const weather = [
   "Just look out the window",
-  "I don't have eye's, how would I know"
+  "I don't have eye's, how would I know",
+  "Just take a look yourself"
 ]
 
+const feelings = [
+  "I cannot feel, I am a computer",
+  "I think I'm feeling hungry, for KNOWLEDGE",
+  "Meh"
+]
+
+const orderResponse = [
+  "Done",
+  "There",
+  "There you are inferior being",
+  "I hate my life",
+  "Whatever"
+]
 // add event listener to btn
 
 btn.addEventListener('click', () => {
   recognition.start();
 });
 
+function smallButton(){
+  console.log(btn.style, "smallButton function");
+  $(".header").height(0);
+  $(".talk").width('10%');
+}
+
 function readOutLoud(message){
+  console.log(message);
   const speech = new SpeechSynthesisUtterance();
   speech.text = message;
 
@@ -45,6 +69,14 @@ function readOutLoud(message){
     console.log('weather');
     const finalText = weather[Math.floor(Math.random() * weather.length)];
     console.log(finalText);
+    speech.text = finalText;
+  } else if (message.includes('how are you feeling')){
+    console.log("feelings");
+    const finalText = feelings[Math.floor(Math.random() * feelings.length)];
+    speech.text = finalText;
+  } else if(message.includes('shrink button')){
+    smallButton();
+    const finalText = orderResponse[Math.floor(Math.random() * orderResponse.length)];
     speech.text = finalText;
   };
 
